0918a3570fa16a71d62162737f4441fa76181c87
Amelie-Manzi <a.gatego@alustudent.com>
2025-01-29 03:41:25 -0800

    check out cohort-2

--- the_coder.sh
+++ the_coder.sh
@@ -6,3 +6,6 @@ touch students_directory/cohort-1.txt
 touch students_directory/cohort-2.txt
 touch students_directory/cohort-3.txt
 	
+echo "simon" >> students_directory/cohort-2.txt
+echo "ella" >> students_directory/cohort-2.txt 
+
